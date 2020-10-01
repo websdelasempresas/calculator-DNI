a23e18230d8d5c4f160468ad47f5fe4f594b6a88
cesar gutierrez <cesargusa22@gmail.com>
2020-10-01 13:13:04 +0200

    Javascript con funcion calcularDNI()

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+function calcularDNI(){
+    let id = document.getElementById('dni');
+    let result = document.getElementById('resultado');
+    let valor = id.value;
+         console.log(valor);
+    let tamano = valor.length;
+    let arrayLetras= ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    let resultado;
+    let numMaxDNI = 8;
+    let numeroDivisor = 23;
+    if(tamano > numMaxDNI || tamano < numMaxDNI){
+        alert('Por favor introduce un codigo de 8 numeros');
+        id.value = " ";
+
+    }else{
+        resultado = (valor % numeroDivisor);
+
+    let total = "DNI: "+valor+arrayLetras[resultado];
+    result.innerHTML = total;
+    }
+    
+    
+    
+}
+
+
+
+
