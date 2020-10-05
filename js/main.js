cdecea72c55f0dc80f847a25f83be2520877fe4d
cesar gutierrez <cesargusa22@gmail.com>
2020-10-05 14:07:42 +0200

    He quitado muchas funciones en main.js que no servian para nada, he añadido codigo para controlar que el resultado sea un numero entero, de esta forma podre controlar que no meten letras o caracteres raros, me ha costado un cojon :=)

--- js/main.js
+++ js/main.js
@@ -1,43 +1,53 @@
 
-    let id = document.getElementById('dni');
-    let cancelar = document.getElementById('cancelar');
-    let result = document.getElementById('resultado');
-    let conta=0;
-function calcularDNI(){
+let id = document.getElementById('dni');
+let cancelar = document.getElementById('cancelar');
+let result = document.getElementById('resultado');
+let conta = 0;
+let contaCancelar = 0;
+function calcularDNI() {
 
     let valor = id.value;
     let tamano = valor.length;
-    let arrayLetras= ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    let arrayLetras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
     let resultado;
     let numMaxDNI = 8;
     let numeroDivisor = 23;
-    if(tamano > numMaxDNI || tamano < numMaxDNI){
-        alert('Por favor introduce un codigo de 8 numeros');
-        id.value = "";
 
-    }else{
+
+
+
+    if (tamano === numMaxDNI) {
         resultado = (valor % numeroDivisor);
-
-            if(conta>0){
-                document.getElementById('resultado').style.visibility='visible';
+        if (resultado > 0) {
+            if (conta > 0) {
+                result.style.visibility = 'visible';
+                cancelar.style.visibility = 'visible';
             }
-    let total = valor + "-" + arrayLetras[resultado];
-    result.innerHTML = total;
-    cancelar.src = 'img/cancelar.png';
+
+            let total = valor + "-" + arrayLetras[resultado];
+            result.innerHTML = total;
+            cancelar.src = 'img/cancelar.png';
+
+        } else {
+            alert("Introduce un dni de 8 digitos sin la letra (;");
+            id.value = "";
+        }
     }
-    
+
 }
 
-function borrarDNI(){
+
+function borrarDNI() {
     conta++;
     let DNI = document.getElementById('dni');
 
     DNI.value = "";
-    result.style.visibility='hidden';
-    
-    
+    result.style.visibility = 'hidden';
+    cancelar.style.visibility = 'hidden';
 
-    
+
+
+
 
 
 }
